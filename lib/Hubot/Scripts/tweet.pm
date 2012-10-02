67ce1b419a9453707f67d1acc673dbfb6df083dd
Hyungsuk Hong <aanoaa@gmail.com>
2012-10-02 17:41:17 +0900

    add `tweet` script for robot `hear` feature testing

---
+++ lib/Hubot/Scripts/tweet.pm
@@ -0,0 +1,46 @@
+package Hubot::Scripts::tweet;
+
+use Moose;
+use namespace::autoclean;
+
+use utf8;
+use URI::Escape;
+use JSON::XS;
+
+has 'robot' => (
+    is  => 'ro',
+    isa => 'Hubot::Robot',
+);
+
+sub BUILD {
+    my $self = shift;
+
+    $self->robot->hear(
+        qr/https?:\/\/(mobile\.)?twitter\.com\/.*?\/status\/([0-9]+)/i,
+        sub {
+            my $msg = shift;    # Hubot::Response
+            $msg->http( 'https://api.twitter.com/1/statuses/show/'
+                  . $msg->match->[1]
+                  . '.json' )->get(
+                sub {
+                    my ( $body, $hdr ) = @_;
+                    return if ( !$body || !$hdr->{Status} =~ /^2/ );
+                    my $tweet = decode_json($body);
+                    $msg->send("$tweet->{user}{screen_name}: $tweet->{text}");
+                }
+                  );
+        }
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+=pod
+
+=head1 SYNOPSIS
+
+just tweet like this `http://twitter.com/pdrucker_bot/status/250419748339331072`
+
+=cut

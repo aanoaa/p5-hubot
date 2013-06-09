fd3928d8ef95795ca2a16e678850e806faa5f0e3
Hyungsuk Hong <aanoaa@gmail.com>
2013-06-09 17:46:25 +0900

    perltidy lib/Hubot/Robot.pm

--- lib/Hubot/Robot.pm
+++ lib/Hubot/Robot.pm
@@ -12,19 +12,19 @@ use Hubot::Brain;
 use Hubot::Listener;
 use Hubot::TextListener;
 
-has 'name' => ( is => 'rw', isa => 'Str' );
+has 'name'  => ( is => 'rw', isa => 'Str' );
 has 'alias' => ( is => 'rw', isa => 'Str' );
-has 'mode' => ( is => 'rw', isa => 'Str', default => '' );
+has 'mode'  => ( is => 'rw', isa => 'Str', default => '' );
 has 'adapter' => ( is => 'rw' );
 has 'brain' => (
-    is => 'ro',
-    isa => 'Hubot::Brain',
+    is      => 'ro',
+    isa     => 'Hubot::Brain',
     default => sub { Hubot::Brain->new }
 );
 has '_helps' => (
     traits  => ['Array'],
-    is => 'rw',
-    isa => 'ArrayRef[Str]',
+    is      => 'rw',
+    isa     => 'ArrayRef[Str]',
     default => sub { [] },
     handles => {
         helps   => 'elements',
@@ -33,8 +33,8 @@ has '_helps' => (
 );
 has '_commands' => (
     traits  => ['Array'],
-    is => 'rw',
-    isa => 'ArrayRef[Str]',
+    is      => 'rw',
+    isa     => 'ArrayRef[Str]',
     default => sub { [] },
     handles => {
         commands   => 'elements',
@@ -43,8 +43,8 @@ has '_commands' => (
 );
 has '_listeners' => (
     traits  => ['Array'],
-    is => 'rw',
-    isa => 'ArrayRef[Hubot::Listener]',
+    is      => 'rw',
+    isa     => 'ArrayRef[Hubot::Listener]',
     default => sub { [] },
     handles => {
         listeners   => 'elements',
@@ -54,34 +54,37 @@ has '_listeners' => (
 
 sub BUILD {
     my $self = shift;
-    $self->loadAdapter($self->adapter)
+    $self->loadAdapter( $self->adapter );
 }
 
 sub loadAdapter {
-    my ($self, $adapter) = @_;
+    my ( $self, $adapter ) = @_;
     ## TODO: HUBOT_DEFAULT_ADAPTERS
     $adapter = "Hubot::Adapter::" . ucfirst($adapter);
     eval "require $adapter; 1";
     if ($@) {
         print STDERR "Cannot load adapter $adapter - $@\n";
-    } else {
-        $self->adapter($adapter->new({ robot => $self }));
+    }
+    else {
+        $self->adapter( $adapter->new( { robot => $self } ) );
     }
 }
 
 sub run { shift->adapter->run }
 
 sub userForId {
-    my ($self, $id, $options) = @_;
+    my ( $self, $id, $options ) = @_;
     my $user = $self->brain->{data}{users}{$id};
     unless ($user) {
-        $user = Hubot::User->new({ id => $id, %$options });
+        $user = Hubot::User->new( { id => $id, %$options } );
         $self->brain->{data}{users}{$id} = $user;
     }
 
     my $options_room = $options->{room} || '';
-    if ($options_room && (!$user->{room} || $user->{room} ne $options_room)) {
-        $user = Hubot::User->new({ id => $id, %$options });
+    if ( $options_room
+        && ( !$user->{room} || $user->{room} ne $options_room ) )
+    {
+        $user = Hubot::User->new( { id => $id, %$options } );
         $self->brain->{data}{users}{$id} = $user;
     }
 
@@ -89,11 +92,11 @@ sub userForId {
 }
 
 sub userForName {
-    my ($self, $name) = @_;
+    my ( $self, $name ) = @_;
     my $result;
-    for my $k (keys %{ $self->brain->{data}{users} }) {
+    for my $k ( keys %{ $self->brain->{data}{users} } ) {
         my $userName = $self->brain->{data}{users}{$k}{name};
-        if (lc $userName eq lc $name) {
+        if ( lc $userName eq lc $name ) {
             $result = $self->brain->{data}{users}{$k};
         }
     }
@@ -102,11 +105,12 @@ sub userForName {
 }
 
 sub usersForFuzzyRawName {
-    my ($self, $fuzzyName) = @_;
+    my ( $self, $fuzzyName ) = @_;
     my $lowerFuzzyName = lc $fuzzyName;
     my @users;
-    while (my ($key, $user) = each %{ $self->brain->{data}{users} || {} }) {
-        if (lc($user->{name}) =~ m/^$lowerFuzzyName/) {
+    while ( my ( $key, $user ) = each %{ $self->brain->{data}{users} || {} } )
+    {
+        if ( lc( $user->{name} ) =~ m/^$lowerFuzzyName/ ) {
             push @users, $user;
         }
     }
@@ -115,11 +119,11 @@ sub usersForFuzzyRawName {
 }
 
 sub usersForFuzzyName {
-    my ($self, $fuzzyName) = @_;
-    my @matchedUsers = $self->usersForFuzzyRawName($fuzzyName);
+    my ( $self, $fuzzyName ) = @_;
+    my @matchedUsers   = $self->usersForFuzzyRawName($fuzzyName);
     my $lowerFuzzyName = lc $fuzzyName;
     for my $user (@matchedUsers) {
-        return $user if lc($user->{name}) eq $lowerFuzzyName;
+        return $user if lc( $user->{name} ) eq $lowerFuzzyName;
     }
 
     return @matchedUsers;
@@ -132,7 +136,7 @@ sub shutdown {
 }
 
 sub loadHubotScripts {
-    my ($self, $scripts) = @_;
+    my ( $self, $scripts ) = @_;
     ## TODO: Debug Message
     # print "Loading hubot-scripts\n" if $ENV{DEBUG};
     for my $script (@$scripts) {
@@ -141,68 +145,73 @@ sub loadHubotScripts {
 }
 
 sub loadFile {
-    my ($self, $script) = @_;
+    my ( $self, $script ) = @_;
     my $full = "Hubot::Scripts::$script";
     eval "require $full; 1";
     $full->load($self);
     if ($@) {
         print STDERR "Unable to load $full: $@\n";
-    } else {
+    }
+    else {
         $self->parseHelp($full);
     }
 }
 
 sub parseHelp {
-    my ($self, $module) = @_;
+    my ( $self, $module ) = @_;
     $module =~ s{::}{/}g;
-    my $fullpath = $INC{$module . '.pm'};
+    my $fullpath = $INC{ $module . '.pm' };
 
     open my $fh, '>', \my $usage or die "Couldn't open filehandle: $!\n";
-    pod2usage({
-        -input   => $fullpath,
-        -output  => $fh,
-        -exitval => 'noexit',
-    });
+    pod2usage(
+        {   -input   => $fullpath,
+            -output  => $fh,
+            -exitval => 'noexit',
+        }
+    );
 
     $usage =~ s/^Usage://;
     $usage =~ s/(^\s+|\s+$)//gm;
-    $self->addHelp($_) for split(/\n/, $usage);
+    $self->addHelp($_) for split( /\n/, $usage );
 
     $module =~ s{Hubot/Scripts/}{};
     $self->addCommand($module);
 }
 
 sub hear {
-    my ($self, $regex, $callback) = @_;
-    $self->addListener(new Hubot::TextListener(
-        robot => $self,
-        regex => $regex,
-        callback => $callback
-    ));
+    my ( $self, $regex, $callback ) = @_;
+    $self->addListener(
+        new Hubot::TextListener(
+            robot    => $self,
+            regex    => $regex,
+            callback => $callback
+        )
+    );
 }
 
 sub respond {
-    my ($self, $regex, $callback) = @_;
+    my ( $self, $regex, $callback ) = @_;
 
     my $index = index "$regex", ':';
-    my $stringRegex = substr "$regex", ($index + 1), -1;
+    my $stringRegex = substr "$regex", ( $index + 1 ), -1;
     my $first = substr $stringRegex, 0, 1;
 
     ## TODO: $^ 에 따른 분기; perl version 에 따라서 Regex object 의 modifier 위치가 달라짐
-    my $modifiers = '';
+    my $modifiers    = '';
     my $modifiersLen = $index - 3;
-    if ($modifiersLen > 0 && length $stringRegex > 3) {
-        $modifiers = substr $stringRegex, 3, $modifiersLen
+    if ( $modifiersLen > 0 && length $stringRegex > 3 ) {
+        $modifiers = substr $stringRegex, 3, $modifiersLen;
     }
 
-    if ($first eq '^') {
-        print STDERR "Anchors don't work well with respond, perhaps you want to use 'hear'\n";
+    if ( $first eq '^' ) {
+        print STDERR
+            "Anchors don't work well with respond, perhaps you want to use 'hear'\n";
         print STDERR "The regex in question was $stringRegex\n";
     }
 
     my $newRegex;
     my $name = $self->name;
-    if ($self->alias) {
+    if ( $self->alias ) {
         my $alias = $self->alias;
         $alias =~ s/[-[\]{}()\*+?.,\\^$|#\s]/\\$&/g; # escape alias for regexp
 
@@ -211,78 +220,94 @@ sub respond {
         ## 일단 modifiers 가 있다면 `i` 라고 가정하고 들어감 WTH..
         if ($modifiers) {
             $newRegex = qr/^(?:$alias[:,]?|$name[:,]?)\s*(?:$stringRegex)/i;
-        } else {
-            $newRegex = qr/^(?:$alias[:,]?|$name[:,]?)\s*(?:$stringRegex)/
         }
-    } else {
+        else {
+            $newRegex = qr/^(?:$alias[:,]?|$name[:,]?)\s*(?:$stringRegex)/;
+        }
+    }
+    else {
         if ($modifiers) {
             $newRegex = qr/^(?:$name[:,]?)\s*(?:$stringRegex)/i;
-        } else {
-            $newRegex = qr/^(?:$name[:,]?)\s*(?:$stringRegex)/
+        }
+        else {
+            $newRegex = qr/^(?:$name[:,]?)\s*(?:$stringRegex)/;
         }
     }
 
     print "$newRegex\n" if $ENV{DEBUG};
-    $self->addListener(new Hubot::TextListener(
-        robot => $self,
-        regex => $newRegex,
-        callback => $callback
-    ));
+    $self->addListener(
+        new Hubot::TextListener(
+            robot    => $self,
+            regex    => $newRegex,
+            callback => $callback
+        )
+    );
 }
 
 sub enter {
-    my ($self, $callback) = @_;
-    $self->addListener(Hubot::Listener->new(
-        robot => $self,
-        matcher => sub { ref(shift) eq 'Hubot::EnterMessage' ? 1 : () },
-        callback => $callback
-    ));
+    my ( $self, $callback ) = @_;
+    $self->addListener(
+        Hubot::Listener->new(
+            robot    => $self,
+            matcher  => sub { ref(shift) eq 'Hubot::EnterMessage' ? 1 : () },
+            callback => $callback
+        )
+    );
 }
 
 sub leave {
-    my ($self, $callback) = @_;
-    $self->addListener(Hubot::Listener->new(
-        robot => $self,
-        matcher => sub { ref(shift) eq 'Hubot::LeaveMessage' ? 1 : () },
-        callback => $callback
-    ));
+    my ( $self, $callback ) = @_;
+    $self->addListener(
+        Hubot::Listener->new(
+            robot    => $self,
+            matcher  => sub { ref(shift) eq 'Hubot::LeaveMessage' ? 1 : () },
+            callback => $callback
+        )
+    );
 }
 
 sub whisper {
-    my ($self, $callback) = @_;
-    $self->addListener(Hubot::Listener->new(
-        robot => $self,
-        matcher => sub { ref(shift) eq 'Hubot::WhisperMessage' ? 1 : () },
-        callback => $callback
-    ));
+    my ( $self, $callback ) = @_;
+    $self->addListener(
+        Hubot::Listener->new(
+            robot   => $self,
+            matcher => sub { ref(shift) eq 'Hubot::WhisperMessage' ? 1 : () },
+            callback => $callback
+        )
+    );
 }
 
 sub notice {
-    my ($self, $callback) = @_;
-    $self->addListener(Hubot::Listener->new(
-        robot => $self,
-        matcher => sub { ref(shift) eq 'Hubot::NoticeMessage' ? 1 : () },
-        callback => $callback
-    ));
+    my ( $self, $callback ) = @_;
+    $self->addListener(
+        Hubot::Listener->new(
+            robot    => $self,
+            matcher  => sub { ref(shift) eq 'Hubot::NoticeMessage' ? 1 : () },
+            callback => $callback
+        )
+    );
 }
 
 sub catchAll {
-    my ($self, $callback) = @_;
-    $self->addListener(Hubot::Listener->new(
-        robot => $self,
-        matcher => sub { ref(shift) eq 'Hubot::CatchAllMessage' ? 1 : () },
-        callback => sub {
-            my $msg = shift;
-            $msg->message($msg->message->message);
-            $callback->($msg);
-        }
-    ));
+    my ( $self, $callback ) = @_;
+    $self->addListener(
+        Hubot::Listener->new(
+            robot => $self,
+            matcher =>
+                sub { ref(shift) eq 'Hubot::CatchAllMessage' ? 1 : () },
+            callback => sub {
+                my $msg = shift;
+                $msg->message( $msg->message->message );
+                $callback->($msg);
+            }
+        )
+    );
 }
 
 sub receive {
-    my ($self, $message) = @_;
+    my ( $self, $message ) = @_;
     my $results = [];
-    for my $listener ($self->listeners) {
+    for my $listener ( $self->listeners ) {
         eval $listener->call($message);
         last if $message->done;
         if ($@) {
@@ -291,12 +316,11 @@ sub receive {
         }
     }
 
-    $self->receive(new Hubot::CatchAllMessage(
-        message => $message
-    )) if ref($message) ne 'Hubot::CatchAllMessage';
+    $self->receive( new Hubot::CatchAllMessage( message => $message ) )
+        if ref($message) ne 'Hubot::CatchAllMessage';
 }
 
-sub http { AnyEvent::HTTP::ScopedClient->new($_[1]) }
+sub http { AnyEvent::HTTP::ScopedClient->new( $_[1] ) }
 
 __PACKAGE__->meta->make_immutable;
 
